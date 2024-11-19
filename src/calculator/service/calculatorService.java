a3138e1920886da587f1230e25ac83102f95e2be
DaryaKolosova <darya-kolosova@mail.ru>
2024-11-19 11:12:54 +0300

    Done

---
+++ src/calculator/service/calculatorService.java
@@ -0,0 +1,11 @@
+package calculator.service;
+
+public interface calculatorService {
+    Integer plus(Integer num1, Integer num2);
+
+    Integer minus(Integer num1, Integer num2);
+
+    Integer multiply(Integer num1, Integer num2);
+
+    Integer divide(Integer num1, Integer num2);
+}
